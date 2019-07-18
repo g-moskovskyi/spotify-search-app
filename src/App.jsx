29c32815722108d2b53650abc5ec52a6e6b9c085
george <g.moskovskyi@gmail.com>
2019-07-18 21:27:51 +0300

    authorization functions are divided

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import './App.scss';
 import authorization from '../src/utils/authorization/authorization';
 // import Header from './components/header/header.jsx';
-import Navbar from './components/navbar/navbar.jsx';
+
 export default class App extends React.Component{
   render () {
     authorization();
     return (
       <div className= '_page'>
-        <nav className="nav">
-          It will be Navbar
-          < Navbar / >
-        </nav>
-        <section className="content">
-          <header className="header">It will be Search-form</header>
-          <div className="playlist">It will be Playlist</div>
+        <section className="fav-section">
+          Here will be Favorite List
+        </section>
+        <section className="play-section">
+          <header className="header">Here will be Search-form</header>
+          <div className="list">Here will be Playlist</div>
         </section>
       </div>
     );
